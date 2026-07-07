8964fc6ba6454154a36804bf78ede1f8bbc6c985
Jayden-Yong <jaydenyong28@gmail.com>
2026-07-07 23:10:28 +0800

    fix: changed trasnport from sse to streamable-http

--- server.py
+++ server.py
@@ -6,4 +6,4 @@ mcp = FastMCP("garden-of-memoria", host="0.0.0.0", port=MCP_PORT)
 register_all(mcp)
 
 if __name__ == "__main__":
-    mcp.run(transport="sse")
+    mcp.run(transport="streamable-http")
